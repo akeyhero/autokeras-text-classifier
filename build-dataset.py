b1a2d7dc4428a8584e1db31b2355a1a74edf7b34
akihiro <akihiro@kats.la>
2018-12-13 06:21:18 +0900

    argparse を使って引数を管理

--- build-dataset.py
+++ build-dataset.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-import sentencepiece as spm
-from itertools import cycle, chain
+import argparse
 
-# FIXME: argparse を使う
+from itertools import cycle, chain
+import sentencepiece as spm
+
 ALL_DOCS_PATH       = 'tmp/all.txt'
 POS_DOCS_PATH       = 'tmp/positive.txt'
 NEG_DOCS_PATH       = 'tmp/negative.txt'
@@ -13,9 +14,10 @@ VOCAB_SIZE          = 10000
 MAX_SENTENCE_LENGTH = 8192
 
 def main():
-    build_model()
-    processor = get_processor()
-    with open(POS_DOCS_PATH) as pos_f, open(NEG_DOCS_PATH) as neg_f:
+    args = parse_args()
+    build_model(args.all_docs_path, args.model_out_prefix, args.vocab_size, args.max_sentence_length)
+    processor = get_processor(args.model_out_prefix)
+    with open(args.pos_docs_path) as pos_f, open(args.neg_docs_path) as neg_f:
         print('Tokenizing and writing sentences...')
         positive_sentences = (tokenize(line, processor) for line in pos_f)
         negative_sentences = (tokenize(line, processor) for line in neg_f)
@@ -23,20 +25,20 @@ def main():
             zip(cycle(['p']), positive_sentences),
             zip(cycle(['n']), negative_sentences)
         )
-        write_with_id(DATA_PATH, labeled_sentences, header=['label', 'body'])
+        write_with_id(args.data_out, labeled_sentences, header=['label', 'body'])
     print('Done.')
 
-def build_model():
+def build_model(_input, model_prefix, vocab_size, max_sentence_length):
     spm.SentencePieceTrainer.Train(' '.join([
-        '--input='               + ALL_DOCS_PATH,
-        '--model_prefix='        + MODEL_PATH_PREFIX,
-        '--vocab_size='          + str(VOCAB_SIZE),
-        '--max_sentence_length=' + str(MAX_SENTENCE_LENGTH)
+        '--input='               + _input,
+        '--model_prefix='        + model_prefix,
+        '--vocab_size='          + str(vocab_size),
+        '--max_sentence_length=' + str(max_sentence_length)
     ]))
 
-def get_processor():
+def get_processor(model_path_prefix):
     processor = spm.SentencePieceProcessor()
-    processor.Load(MODEL_PATH_PREFIX + '.model')
+    processor.Load(model_path_prefix + '.model')
     return processor
 
 def write_with_id(path, values_list, header=None, separator="\t"):
@@ -50,6 +52,24 @@ def write_with_id(path, values_list, header=None, separator="\t"):
 
 def tokenize(sentence, processor):
     return ' '.join(processor.EncodeAsPieces(sentence))
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--all-docs-path', type=str, default=ALL_DOCS_PATH, \
+            help='全文書を列挙したもの')
+    parser.add_argument('--pos-docs-path', type=str, default=POS_DOCS_PATH, \
+            help='ポジティブな文書のみを列挙したもの')
+    parser.add_argument('--neg-docs-path', type=str, default=NEG_DOCS_PATH, \
+            help='ネガティブな文書のみを列挙したもの')
+    parser.add_argument('--data-out', type=str, default=DATA_PATH, \
+            help='データ出力先')
+    parser.add_argument('--model-out-prefix', type=str, default=MODEL_PATH_PREFIX, \
+            help='モデル出力先プレフィックス')
+    parser.add_argument('--vocab-size', type=str, default=VOCAB_SIZE, \
+            help='語彙のサイズ')
+    parser.add_argument('--max-sentence-length', type=str, default=MAX_SENTENCE_LENGTH, \
+            help='最大文書長 (bytes)')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 
+import argparse
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
 from classifiers.text_classifier import TextClassifier
 
-# FIXME: argparse を使う
 DATA_PATH = 'tmp/data.tsv'
 RESULT_DIR = 'results'
 
 def main():
-    x_train, y_train = read_csv(DATA_PATH)
-    x_train, x_text, y_train, y_test = train_test_split(x_train, y_train)
-    clf = TextClassifier(path=RESULT_DIR, verbose=True)
-    clf.fit(x_train, y_train, time_limit=12*60*60)
+    args = parse_args()
+    x_train, y_train = read_csv(args.data_path)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train)
+    clf = TextClassifier(max_seq_length=args.max_seq_length,
+                         path=args.result_dir,
+                         resume=args.resume,
+                         verbose=True)
+    clf.fit(x_train, y_train, time_limit=args.time_limit)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
 
 def read_csv(file_path):
@@ -27,6 +33,17 @@ def read_csv(file_path):
     x_train = np.array(data_train.body)
     y_train = np.array(data_train.label)
     return x_train, y_train
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data-path', type=str, default=DATA_PATH, \
+            help='データセット (id, label, body の TSV) のパス')
+    parser.add_argument('--result-dir', type=str, default=RESULT_DIR, \
+            help='結果出力ディレクトリ (--resume の際はやり直し前後でディレクトリを変更できません)')
+    parser.add_argument('--resume', action='store_true', help='途中からやり直す')
+    parser.add_argument('--max-seq-length', type=int, help='最長単語数')
+    parser.add_argument('--time-limit', type=int, default=12*60*60, help='学習時間 (s)')
+    return parser.parse_args()
 
 if __name__ == '__main__':
     main()
